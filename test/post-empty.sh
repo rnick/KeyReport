49bccb39d8b0de6ef3cdf62f16207e1d06edd565
Ralf Nickel <rn@itrn.de>
2021-10-19 12:59:59 +0200

    remove unused tests

--- test/post-empty.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-curl -v -H "Content-Type: application/json" -d "@../data/beispiel-empty.json" -X POST http://127.0.0.1/intern/erep/report.php --output report-curl-radar-empty.pdf 
--- test/post-ref.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-curl -v -H "Content-Type: application/json" -d "@../data/beispiel-ref.json" -X POST http://127.0.0.1/erep/report.php --output report-curl-ref.pdf 
